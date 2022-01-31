c267dfe232f06d2c7312e958548df21c95b8d068
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2022-01-31 15:42:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -37,3 +37,29 @@ function draw() {
         document.getElementById('score').innerHTML = 'Score: ' + score;
     }
 }
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
+    document.getElementById('confidence').innerHTML = 'Confidence ' + Math.round(results[0].confidence * 100) + '%';
+}
+function check_sketch() {
+    timer_counter++;
+    document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
+    console.log(timer_counter);
+    if (timer_counter > 400) {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if (timer_check == "completed" || answer_holder == "set") {
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
